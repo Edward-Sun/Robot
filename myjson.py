fa200b71b997d62256ad1a61c40f396e0b1019e4
Edward Sun <569342215@qq.com>
2018-01-22 23:46:55 +0800

    clean

--- myjson.py
+++
@@ -1,8 +0,0 @@
-import json
-import requests
-data = {'key':'50d6624f41424826807103b1a76a8f6e',
-		'info' : '除非我亲吻青蛙大全无法前往',
-		'userid' : '123456'}
-r = requests.post('http://www.tuling123.com/openapi/api', data=data)
-print(r.json())
-print(r.json()['text'])
